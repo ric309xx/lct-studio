5e131b6167f2dd09d8c27155fba5711202087d0e
ric309xx <ric309xx@gmail.com>
2026-09-12 17:38:11 +0800

    Update Longteng archive video

--- archive/archive.js
+++ archive/archive.js
@@ -89,7 +89,7 @@
       films: [{
         id: "longteng-model",
         title: "龍騰斷橋模型飛行模擬",
-        youtubeId: "lG7icG_jwsU",
+        youtubeId: "gBcufs_Y2Sw",
         poster: `${PHOTO_BASE}苗栗縣三義鄉龍騰斷橋 (2).jpg`
       }],
       photos: selectedPhotos("苗栗縣三義鄉龍騰斷橋", [1, 2, 3])
